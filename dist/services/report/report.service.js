f6822af372fc741ce764adcb8479dcec21fd5e79
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-12-13 19:25:45 -0300

    fix: refactor Puppeteer import to use dynamic loading for improved performance

--- dist/services/report/report.service.js
+++ dist/services/report/report.service.js
@@ -1,10 +1,39 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/services/report/report.service.ts
-const puppeteer_1 = __importDefault(require("puppeteer"));
 const report_template_1 = require("../../template/report.template");
 class ReportService {
     constructor() {
@@ -27,9 +56,10 @@ class ReportService {
      * Gera PDF a partir do HTML usando Puppeteer
      */
     async generatePDF(html) {
-        const browser = await puppeteer_1.default.launch({
+        const puppeteer = await Promise.resolve().then(() => __importStar(require("puppeteer")));
+        const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
         });
         try {
             const page = await browser.newPage();
